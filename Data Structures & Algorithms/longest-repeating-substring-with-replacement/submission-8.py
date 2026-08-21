f6d6f9a7684e41958a3bcbff19d152b00c8b435d
RakshithaMurthy <35700896+RakshithaMurthy@users.noreply.github.com>
2026-08-21 04:49:27 -0400

    Add: longest-repeating-substring-with-replacement - submission-8

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-8.py
@@ -0,0 +1,111 @@
+from collections import defaultdict
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        l = 0
+        max_freq = 0
+        res = 0
+
+        for r in range(len(s)):
+            count[s[r]] += 1
+            max_freq = max(max_freq, count[s[r]])
+
+            # window not valid
+            #current window size = (r - l + 1)
+            #replacements needed = window size - count of most frequent character
+            while (r - l + 1) - max_freq > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res
+
+#Longest substring with at most K distinct characters
+# from collections import defaultdict
+
+# class Solution:
+#     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+#         if k == 0:
+#             return 0
+
+#         count = defaultdict(int)
+#         l = 0
+#         res = 0
+
+#         for r in range(len(s)):
+#             count[s[r]] += 1
+
+#             while len(count) > k:
+#                 count[s[l]] -= 1
+#                 if count[s[l]] == 0:
+#                     del count[s[l]]
+#                 l += 1
+
+#             res = max(res, r - l + 1)
+
+#         return res
+
+# #Longest substring with exactly K distinct characters
+
+# from collections import defaultdict
+
+# class Solution:
+#     def longestKDistinct(self, s: str, k: int) -> int:
+#         count = defaultdict(int)
+#         l = 0
+#         res = -1 #no valid substring
+
+#         for r in range(len(s)):
+#             count[s[r]] += 1
+
+#             while len(count) > k:
+#                 count[s[l]] -= 1
+#                 if count[s[l]] == 0:
+#                     del count[s[l]]
+#                 l += 1
+
+#             if len(count) == k:
+#                 res = max(res, r - l + 1)
+
+#         return res
+'''
+| Problem   | Condition                                       |
+| --------- | ----------------------------------------------- |
+| At most K | keep `≤ k`                                      |
+| Exactly K | shrink if `> k`, update answer only when `== k` |
+
+'''
+
+'''
+#Number of distinct elements in window of size K
+
+from collections import defaultdict
+
+class Solution:
+    def countDistinct(self, nums, k):
+        freq = defaultdict(int)
+        res = []
+        distinct = 0
+
+        l = 0
+
+        for r in range(len(nums)):
+            freq[nums[r]] += 1
+            if freq[nums[r]] == 1:
+                distinct += 1
+
+            # shrink window when size exceeds k
+            if r - l + 1 > k:
+                freq[nums[l]] -= 1
+                if freq[nums[l]] == 0:
+                    distinct -= 1
+                l += 1
+
+            if r - l + 1 == k:
+                res.append(distinct)
+
+        return res
+
+    '''
